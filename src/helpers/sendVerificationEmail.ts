0e63d5829cc91309db669947938342c1175587d9
sahilvishwa2108 <sahilvishwa2108@gmail.com>
2025-02-19 01:01:27 +0530

    Add signup route and email verification functionality with bcrypt integration

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,6 +1,24 @@
-import { Resend } from "resend";
+import {resend} from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
-import { ApiResponse } from "@/types/apiResponse";
+import { ApiResponse } from "@/types/ApiResponse";
+import { url } from "inspector";
 
-export async function sendVerificationEmail(email: string): Promise<ApiResponse> {
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifycode: string,
+): Promise<ApiResponse> {
+	try {
+        await resend.emails.send({
+            from: "you@example.com",
+            to: email,
+            subject: "Verify your email",
+            react: VerificationEmail({username, otp: verifycode}),
+        });
+        return ({success: true, message: "Verification email sent"});
+    } catch (emailError) {
+        console.error("Error sending verification email", emailError);
+        return {success: false, message: "Error sending verification email"};
+    }
+}
     
